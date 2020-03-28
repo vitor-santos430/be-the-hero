f4cdbf283736efd6936bc326f21546b668cb18e8
Vitor Santos <vitor.santos430@etec.sp.gov.br>
2020-03-28 15:36:47 -0300

    Testes de aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
 const express = require('express'); // importando o Express
-
 const cors = require('cors');
-
 const routes = require('./routes')
+const { errors } = require('celebrate');
 
 const app  = express(); // instancia da aplicação
 
 app.use(cors());
 app.use(express.json()); // Dizer para o express que estamos utilizando JSON
 app.use(routes);
+app.use(errors())
 
-app.listen(3333); //  diz que o app está ouvindo na porta 3333
+module.exports = app;
+
+//app.listen(3333); //  diz que o app está ouvindo na porta 3333
 // para executar o servidor 'node index.js'
